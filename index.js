4589e32c0d2bb3ea9dff9627490fb97985da6fdc
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-13 06:30:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object [key] = value
+  object [key] = value;
   return object;
 }
 function deleteFromObjectByKey(object, key){
-  Object.assign ({}, obj);
+  Object.assign ({}, object);
   delete object.key;
   return object;
 }
