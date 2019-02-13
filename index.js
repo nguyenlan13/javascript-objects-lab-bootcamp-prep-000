c34bab8f53a0578f662a0842955d74ee029232ba
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-13 06:06:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ var recipes = {recipes: 'object'};
 function updateObjectWithKeyAndValue(object, key, value){
 
   return Object.assign ({}, {[key]: value});
+}
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
 }
